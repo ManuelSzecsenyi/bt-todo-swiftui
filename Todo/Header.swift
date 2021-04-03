575f1200c37a6faa0a711bd6b81e13400cd0c73c
Manuel Szecsenyi <manuel.szecsenyi@gmail.com>
2021-04-03 21:21:23 +0200

    added date and headings to header

--- Todo/Header.swift
+++ Todo/Header.swift
@@ -13,24 +13,34 @@ struct Header: View {
 	private var url = URL(string:  "https://images.pexels.com/photos/5325834/pexels-photo-5325834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
 	
     var body: some View {
-		HStack {
-			Image(systemName: "circle.grid.2x2.fill")
-				.font(.system(size: 26))
-			Spacer()
-			Text("ToDo")
-				.bold()
-				.font(.system(size: 26))
-			Spacer()
-			URLImage(url: self.url!,
-			 content: { image in
-				 image
-					.resizable()
-					.aspectRatio(contentMode: .fill)
-					.frame(width: 40.0, height: 40.0)
-					.clipShape(Circle())
-			 })
+		VStack(alignment: .leading ) {
+			HStack {
+				Image(systemName: "circle.grid.2x2.fill")
+					.font(.system(size: 26))
+				Spacer()
+				Text("ToDo")
+					.bold()
+					.font(.system(size: 26))
+				Spacer()
+				URLImage(url: self.url!,
+				 content: { image in
+					 image
+						.resizable()
+						.aspectRatio(contentMode: .fill)
+						.frame(width: 40.0, height: 40.0)
+						.clipShape(Circle())
+				 })
+				
+			}
 			
+			Text(DateFormats.heeadingFormat.string(for: Date())!)
+				.font(.system(size: 34.0))
+				.bold()
+			Text("Meine Aufgaben")
+				.font(.system(size: 19))
+				.bold()
 		}
+		
         
     }
 }
@@ -39,4 +49,17 @@ struct Header_Previews: PreviewProvider {
     static var previews: some View {
         Header()
     }
+}
+
+
+
+struct DateFormats {
+
+	static var heeadingFormat: DateFormatter = {
+		let df = DateFormatter()
+		df.dateFormat = "EEE MM.dd.YY"
+		df.locale = Locale(identifier: "de")
+		return df
+	}()
+
 }
