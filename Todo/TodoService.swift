2f8507fc01c345cff2077f35f9d59dc850321c90
Manuel Szecsenyi <manuel.szecsenyi@gmail.com>
2021-04-04 17:02:03 +0200

    data successfully fetched

--- Todo/TodoService.swift
+++ Todo/TodoService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 struct Response: Codable {
 	var items: [TodoItemModel]
@@ -18,32 +19,46 @@ struct TodoItemModel: Codable {
 	var createdAt: Date
 }
 
+enum DateError: String, Error {
+	case invalidDate
+}
+
 class TodoService {
 	
-	let url = URL(string: "http://localhost:3000")
+	static let url = "http://localhost:3000"
 	
 	static func getItems() {
-		let request = URLRequest(url: URL(string: "http://localhost:3000")!)
 		
-		print("load")
-		
-		URLSession.shared.dataTask(with: request) { data, response, error in
-		
-			if let data = data {
-				if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-					// we have good data – go back to the main thread
-					DispatchQueue.main.async {
-						// update our UI
-						print(decodedResponse)
-						print("load finished")
-					}
+		AF.request(TodoService.url).responseData { response in
+			switch response.result {
+				case .failure(let error):
+					print(error)
+				case .success(let data):
+					// custom decoder needed https://stackoverflow.com/questions/46537790/iso8601-date-json-decoding-using-swift4
+					let decoder = JSONDecoder()
+					let formatter = DateFormatter()
+					decoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
+						let container = try decoder.singleValueContainer()
+						let dateStr = try container.decode(String.self)
 
-					// everything is good, so we can exit
-					return
-				}
+						formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+						if let date = formatter.date(from: dateStr) {
+							return date
+						}
+						formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
+						if let date = formatter.date(from: dateStr) {
+							return date
+						}
+						throw DateError.invalidDate
+					})
+					do {
+						let pageData = try decoder.decode([TodoItemModel].self, from: data)
+						print(pageData)
+					} catch let error {
+						print(error)
+					}
 			}
-		
-		}.resume()
+		}
 	}
 	
 }
